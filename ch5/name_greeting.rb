2024b23e9b7b2daddaedd0c5396fe7e5ae87b039
Neel <neelam.sunwar@gmail.com>
2016-06-28 20:11:35 +0100

    Completed name_greeting

---
+++ ch5/name_greeting.rb
@@ -0,0 +1,8 @@
+puts "Hello there, what\'s your first name?"
+f_name = gets.chomp
+puts "What\'s your middle name?"
+m_name = gets.chomp
+puts "What\'s your last name?"
+l_name = gets.chomp
+full_name = f_name + ' ' + m_name + ' ' + l_name
+puts "Nice to meet you " + full_name + "."
