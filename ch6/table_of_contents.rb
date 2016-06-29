316917cd90b9422ab292f430b4f8f5ae12382b6e
Neel <neelam.sunwar@gmail.com>
2016-06-29 12:26:33 +0100

    Completed table_of_contents

---
+++ ch6/table_of_contents.rb
@@ -0,0 +1,6 @@
+line_width= 50
+puts 'Table of Contents'.center(line_width)
+puts ''
+puts 'Chapter 1 : Getting Started'.ljust(30) + 'Page 1'.rjust(20)
+puts 'Chapter 2 : Numbers'.ljust(30) + 'Page 9'.rjust(20)
+puts 'Chapter 3 : Letters'.ljust(30) + 'Page 13'.rjust(20)
